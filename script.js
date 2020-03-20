c8a4429fba4784e92aee1897c9b033c4c26cc68e
Andrew Kessler <andrew.kessler95@gmail.com>
2020-03-20 12:11:41 -0600

    changed the location of the user input boxes

--- script.js
+++ script.js
@@ -17,10 +17,12 @@ const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
 ).concat(
   arrayFromLowToHigh(123, 126)
 )
+//Event Listener for making the range bar and the number input field in sync 
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
 
+//form.addEventListener loooking to see; user password length input, user criteria input (if satalites are checked for upper, numbers and special characters) 
 form.addEventListener('submit', e => {
   e.preventDefault()
   const characterAmount = characterAmountNumber.value
@@ -34,6 +36,7 @@ form.addEventListener('submit', e => {
 
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
   console.log("text")
+  // Defult password will only use the lower case letter array if statements will add user input 
   let charCodes = LOWERCASE_CHAR_CODES
   if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
   if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
@@ -44,11 +47,12 @@ function generatePassword(characterAmount, includeUppercase, includeNumbers, inc
     const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
+  // join is converting the generated password into a string 
   return passwordCharacters.join('')
 }
 
 
-// function to create array using char codes 
+// function to create array using char codes variable from lines 9-19 
 function arrayFromLowToHigh(low, high) {
   const array = []
   for (let i = low; i <= high; i++) {
@@ -58,7 +62,7 @@ function arrayFromLowToHigh(low, high) {
 }
 
 
-//making the range bar and the number input field in sync 
+// Function making the range bar and the number input field in sync 
 function syncCharacterAmount(e) {
   const value = e.target.value
   characterAmountNumber.value = value
